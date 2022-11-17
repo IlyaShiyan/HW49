5725f5f379b8a6680ea6d6d7d39e8fb247a50330
Ilya Shiyan <ilya.shiyan97@mail.ru>
2022-11-17 07:39:44 +0300

    Finished project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,29 +4,28 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> listOfIntegersEven = new ArrayList<>();
+        List<Integer> listOfIntegers = new ArrayList<>();
         List<Integer> listOfIntegersOdd = new ArrayList<>();
-        listOfIntegersEven.add(5);
-        listOfIntegersEven.add(7);
-        listOfIntegersEven.add(8);
-        listOfIntegersEven.add(4);
-        listOfIntegersEven.add(3);
+        List<Integer> listOfIntegersEven = new ArrayList<>();
+        listOfIntegers.add(5);
+        listOfIntegers.add(7);
+        listOfIntegers.add(8);
+        listOfIntegers.add(4);
+        listOfIntegers.add(3);
 
-        for (Integer i: listOfIntegersEven) {
-            if(i%2 != 0){
+        for (Integer i: listOfIntegers) {
+            if(i%2 != 0)
                 listOfIntegersOdd.add(i);
-            }
-        }
-
-        Iterator<Integer> listOfIntegersIterator = listOfIntegersEven.iterator();
-        while (listOfIntegersIterator.hasNext()){
-            Integer nextInteger = listOfIntegersIterator.next();
-            if(nextInteger%2 != 0){
-                listOfIntegersIterator.remove();
-            }
+            else
+                listOfIntegersEven.add(i);
         }
 
         System.out.println(listOfIntegersEven);
         System.out.println(listOfIntegersOdd);
+
+        if (listOfIntegersEven.size()>listOfIntegersOdd.size())
+            System.out.println("The list of even numbers is greater than the list of odd numbers");
+        else
+            System.out.println("The list of odd numbers is greater than the list of even numbers");
     }
 }
